93bd671c4120a5569524a29e293f5b253f67b947
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-07-20 13:38:08 -0700

    using hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [search, setSearch] = useState();
+
+  const handleInput = (event) => {
+    console.log(event.target.value)
+    setSearch(event.target.value);
+
+  }
+
+  const myFun = async () => {
+    const get = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${search}`);
+    const jsonData = await get.json();
+    console.log(get);
+
+  }
   return (
     <div className='app'>
       <h1 className='heading'>Dictionary App</h1>
       <div className='container'>
         <div className='search-bar'>
-          <input type='text' placeholder='Search for a word...' />
-          <button>Search</button>
+          <input type='text' placeholder='Search for a word...' onChange={handleInput} />
+          <button onClick={myFun}>Search</button>
         </div>
         <div className='results'></div>
 
