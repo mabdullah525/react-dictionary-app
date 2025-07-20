c96b35cb441908c01f355f51e901d8e8bdb73643
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-07-20 08:37:50 -0700

    Add style

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div className='app'>
-      <h1>Dictionary App</h1>
+      <h1 className='heading'>Dictionary App</h1>
       <div className='container'>
         <div className='search-bar'>
           <input type='text' placeholder='Search for a word...' />
