1861233a990986383f85cacb5a721fe6795b1b8f
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-07-20 08:31:32 -0700

    Starting work

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+      <h1>Dictionary App</h1>
+      <div className='container'>
+        <div className='search-bar'>
+          <input type='text' placeholder='Search for a word...' />
+        </div>
+
+      </div>
+    </div>
   )
 }
 
